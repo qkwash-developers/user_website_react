094fb7f0be58059327dc5a7c64c741768cd2846e
Sreejith U <126107363+sreejithdev2002@users.noreply.github.com>
2025-02-24 14:30:05 +0530

    Made a small change

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,15 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { ToastContainer } from "react-toastify";
 
-import App from './App';
-import { ToastContainer } from 'react-toastify';
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-  <React.StrictMode>
-    <App />
-    <ToastContainer/>
-  </React.StrictMode>
+    <React.StrictMode>
+      <App />
+      <ToastContainer />
+    </React.StrictMode>
   </BrowserRouter>
 );
